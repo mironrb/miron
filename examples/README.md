a59091fba5fb2e815b37f9a9c050f0a20b2ac635
maclover7 <me@jonathanmoss.me>
2015-10-13 18:54:54 -0400

    Add a middleware example

---
+++ examples/README.md
@@ -0,0 +1,6 @@
+# Examples
+
+- See [app.rb](app.rb) for an example of a regular old Miron app, with
+  no middleware.
+- See [middleware.rb](middleware.rb) for an example of an application
+  with middleware.
---
+++ examples/middleware.rb
@@ -0,0 +1,28 @@
+# Here's an example of just a regular old Miron app.
+# This code should go into a `Mironfile.rb`.
+
+class AppMiddleware
+  def call(request, response)
+    # When AppMiddleware is called, hi will be puts'ed
+    puts 'hi'
+  end
+end
+
+class App
+  def call(request, response)
+    # Set your Integer HTTP status here.
+    response.http_status = 200
+    # Set your Hash of HTTP headers here.
+    response.headers = {}
+    # Set your String of HTTP body here.
+    response.body = 'hi'
+    # Set your Hash of HTTP cookies here.
+    # Don't worry about `Set-Cookie`, that's handled down the line for you. :)
+    response.cookies = { 'HELLO' => 'HELLO' }
+    # Finally, return the response upstream for delivery.
+    return response
+  end
+end
+
+use Middleware
+run App
